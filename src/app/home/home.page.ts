e93f3da7090535111d0c6e464ffb5974db996a84
ahmad ade rozak <ahmadrozak@versatech.co.id>
2023-05-25 13:12:01 +0700

    initial
    Signed-off-by: ahmad ade rozak <ahmadrozak@versatech.co.id>

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private home_timer: number = 0;
 
-  constructor() {}
+  constructor(
+    private loadingController: LoadingController,
+    private router : Router
+  ) {}
 
+  private timer_count (params:any) {
+    return params * 1000
+  }
+
+  private async checkSession() {
+    clearTimeout(this.home_timer);
+    let loading = await this.loadingController.create({
+      message: 'Please wait...',
+    });
+    this.home_timer = setTimeout(() =>{
+      loading.present();
+      console.log('present');
+    }, this.timer_count(2))
+
+    this.home_timer = setTimeout(() =>{
+      loading.dismiss();
+      console.log('dismiss');
+      this.router.navigate(['login-page'])
+    }, this.timer_count(8))
+  }
+
+  ngOnInit(){
+    this.checkSession().then(res =>{
+      console.log(res)
+    });
+
+  }
 }
